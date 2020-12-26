53e54b8582eee9018832d89816b4f6591bc10a69
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-26 16:02:45 +0530

    [Arti Dandge] Add : Ability to edit existing contats using person name

---
+++ EditContactsUsingFirstName.sql
@@ -0,0 +1,8 @@
+update [dbo].[address_book] set city = 'Mumbai' 
+where first_name = 'Arti';
+
+update [dbo].[address_book] set phone_number = '7776543215'
+where first_name = 'Ashwini';
+
+update [dbo].[address_book] set last_name = 'kushwaha'
+where first_name = 'Preeti';

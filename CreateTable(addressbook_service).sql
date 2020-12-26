53783f3c77f6e3cf8d69632094f36923ee5b90e8
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-26 15:31:14 +0530

    [Arti Dandge] Add : Ability to create Address Book Table with first and last name, address, city, state, zip, phon_number and email as its attributes

---
+++ CreateTable(addressbook_service).sql
@@ -0,0 +1,11 @@
+create table address_book 
+(
+first_name varchar(20) not null,
+last_name varchar(20) not null,
+address varchar(20) not null,
+city varchar(15) not null,
+state varchar(15) not null,
+zip int not null,
+phone_number varchar(10) not null,
+email varchar(20) not null 
+)

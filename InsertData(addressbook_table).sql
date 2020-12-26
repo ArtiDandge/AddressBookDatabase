1978c8e5bb74436e9e344df055c3eafa7baecb7a
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-26 15:43:57 +0530

    [Arti Dandge] Add : Ability to insert new Contacts to AddressBook

---
+++ InsertData(addressbook_table).sql
@@ -0,0 +1,5 @@
+insert into [dbo].[address_book] values
+('Arti', 'Dandge', 'Ashish Plaza', 'Pune', 'Maharashtra', 412233, '9459454954', 'asd@gmail.com'),
+('Sneha', 'Rathod', 'Shahu Nagar', 'Satara', 'Maharashtra', 424333, '7443443434', 'sneha@gmail.com'),
+('Ashwini', 'Bhutke', 'Reman Chouk', 'Pune', 'Maharashtra', 400003, '8889454954', 'ashwini@gmail.com'),
+('Preeti', 'kushwah', 'Venu Hights', 'Chinchawad', 'Maharashtra', 422233, '8458765954', 'preeti@gmail.com')

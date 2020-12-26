3936c172ecfb1277b2256e1f9a4b859cd6996ccc
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-26 18:35:12 +0530

    [Arti Dandge] Add : Ability to person to both Friend and Family

---
+++ InsertNewPersonInSpecificType.sql
@@ -0,0 +1,4 @@
+insert into [dbo].[address_book] values
+('Sejal', 'Dandge', 'latur', 'latur', 'MH', 413553, '9876543210', 'sejal@yahoo.com', 'AddressBook2', 'Family'),
+('Komal', 'Sake', 'Satara', 'Satara', 'MH', 410055, '9876577760', 'komal@yahoo.com', 'AddressBook4', 'Friends'),
+('Rama', 'Magare', 'Indor', 'Indor', 'MP', 411155, '9878888510', 'Rama@yahoo.com', 'AddressBook1', 'Family')

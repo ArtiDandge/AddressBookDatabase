7bdf2c86c5267ad804c6621566629be8a84f3de0
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-26 16:46:23 +0530

    [Arti Dandge] Add : Ability to retrieve entries sorted alphabetically by person's name for a given city

---
+++ SortPersonNamesAlphabetically.sql
@@ -0,0 +1,5 @@
+select first_name, last_name from [dbo].[address_book]
+where city = 'Pune' order by first_name asc;
+
+select first_name, last_name from [dbo].[address_book]
+where city = 'Mumbai' order by first_name asc;
